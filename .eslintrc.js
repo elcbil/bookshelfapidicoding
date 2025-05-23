d43d374a6e2fafbf6aa4e4ec8085ea75099375be
elcbil <nabilahdaa@gmail.com>
2025-05-23 22:50:47 +0700

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    'no-console': 'off',
+    'indent': ['error', 2],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'always'],
+    'no-unused-vars': 'error',
+    'no-undef': 'error',
+  },
+};
